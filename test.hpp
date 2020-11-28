46d606186429adea38966066673fcac3a8d527cb
Chrisdes-99 <desantiagochris18@gmail.com>
2020-11-28 08:56:52 -0800

    Essential Files Created

---
+++ test.hpp
@@ -0,0 +1,16 @@
+#ifndef __TEST_HPP__
+#define __TEST_HPP__
+
+#include <iostream>
+#include "gtest/gtest.h"
+
+#include "BaseFactory.hpp"
+
+//Base Factory Unit Tests
+
+
+
+
+
+
+#endif //__TEST_HPP__
