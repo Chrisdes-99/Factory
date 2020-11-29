45db589728d8852fd707a18221c0333d68c2fbf6
Chrisdes-99 <desantiagochris18@gmail.com>
2020-11-29 09:02:06 -0800

    Very Minor Additions for BaseFactory functionality

--- test.hpp
+++ test.hpp
@@ -6,7 +6,14 @@
 
 #include "BaseFactory.hpp"
 
-//Base Factory Unit Tests
+#include "./Lab-04-Composite/add.hpp"
+#include "./Lab-04-Composite/pow.hpp"
+#include "./Lab-04-Composite/sub.hpp"
+#include "./Lab-04-Composite/op.hpp"
+#include "./Lab-04-Composite/rand.hpp"
+#include "./Lab-04-Composite/mult.hpp"
+#include "./Lab-04-Composite/div.hpp"
+
 
 
 
