a785b629e7ee1d99262cd6ec714084392695e28d
Chrisdes-99 <desantiagochris18@gmail.com>
2020-11-29 21:27:58 -0800

    Parse Layout Created,Not Real Definition

--- BaseFactory.cpp
+++ BaseFactory.cpp
@@ -2,8 +2,27 @@
 
 Base* BaseFactory::parse(char** input,int length)
 {
+  //Intended not as real definition
+  //Only layout
+  
+  //sstream
   for(int i= 0; i < length;i++){
-        
- }
+      if(input == "*"){
+        Mult(this->left,this->right);
+      } 
+      else if(input == "/"){
+        Div(this->left,this->right);
+      }
+      else if(input == "+"{
+        Add(this->left,this->right);
+      }
+      else if(input == "-"){
+        Sub(this->left,this->right);
+      }
+      else{
+        cout<<"Invalid Input"<<endl;
+      }
+   }  
+  return this->root->evaluate()
 }
 
